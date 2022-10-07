4baac44aa916bd97b40dd7423741b43768568736
SamuelAndresRojas <saanrojas@gmail.com>
2022-10-07 21:20:55 +1100

    Progress

--- src/Assigment3.java
+++ src/Assigment3.java
@@ -1,8 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Assigment3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			
+		BufferedReader fileReader = null;
+		String txt = "";
+		
+		try {
+			fileReader = new BufferedReader(new FileReader("data.txt"));
+			
+			while((txt = fileReader.readLine()) != null){
+//				System.out.println(txt);
+				
+			}
+		
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
