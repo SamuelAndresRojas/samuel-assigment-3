8e347812bffccab2d53025fd73903d3bef6445c3
SamuelAndresRojas <saanrojas@gmail.com>
2022-10-11 22:49:35 +1100

    New Update

--- src/Assigment3.java
+++ src/Assigment3.java
@@ -6,32 +6,36 @@ import java.util.Scanner;
 
 public class Assigment3 {
 
+	@SuppressWarnings("null")
 	public static void main(String[] args) throws IOException {
 
 		User[] users = new User[4];
 		UserService userService = new UserService();
 		BufferedReader fileReader = null;
-		String txt = "";
+		String txt;
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Welcome");
-		System.out.println("Enter your Email:");
-		String emailInput = scanner.nextLine();
-		System.out.println("Enter your Password:");
-		String passwordInput = scanner.nextLine();
 
 		fileReader = new BufferedReader(new FileReader("data.txt"));
 
-		int i = 0;
-		int tries = 0;
-		while (tries < 2) {
 			while ((txt = fileReader.readLine()) != null) {
+				
+			}
+			
+			int i = 0;
+			int tries = 0;
+			while (tries < 2) {
+				
 				String[] values = txt.split(",");
 				User user = userService.createUser(values[0], values[1], values[2]);
-
 				users[i] = user;
 				i++;
-
+				System.out.println("Enter your Email:");
+				String emailInput = scanner.nextLine();
+				System.out.println("Enter your Password:");
+				String passwordInput = scanner.nextLine();
+						
 				if (emailInput.equalsIgnoreCase(values[0]) && passwordInput.equals(values[1])) {
 					System.out.println("Welcome: " + values[2]);
 				} else {
@@ -47,5 +51,3 @@ public class Assigment3 {
 		}
 
 	}
-
-}
